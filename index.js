6fff1643956e41893620bdee65d2afbb69d997ac
Akshat Biniwale <akshatbiniwale@gmail.com>
2023-05-15 22:46:43 +0530

    Challenge 9

--- index.js
+++ index.js
@@ -10,23 +10,35 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const postContent = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home", {homeStartingContent: homeStartingContent, postContent: postContent});
+});
 
+app.post("/", function(req, res){
+  var post = req.body.post;
+  postContent.push(post);
 
+  res.redirect("/");
+})
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
-
-
-
-
-
-
-
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
